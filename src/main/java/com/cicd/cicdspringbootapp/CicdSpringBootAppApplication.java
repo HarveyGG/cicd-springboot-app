a64e9fb943eab04e8d2c9866d4debdad96c51869
harvey.wanghy <harvey.wanghy@alibaba-inc.com>
2019-11-11 15:54:03 +0800

    init

---
+++ src/main/java/com/cicd/cicdspringbootapp/CicdSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.cicd.cicdspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSpringBootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdSpringBootAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cicd/cicdspringbootapp/CicdSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.cicdspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSpringBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
